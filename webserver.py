b73991aa3fffc6bf2282d17c5351a721641a6a6e
Paul Mullins <paulmullins@protonmail.com>
2020-03-28 22:39:46 -0400

    Added button to reset cookie, and set cookie to /

--- webserver.py
+++ webserver.py
@@ -9,12 +9,6 @@ app = Flask(__name__)
 TEMPLATES_AUTO_RELOAD = True
 #app.config['SECRET_KEY'] =  'e5ac358c-f0bf-11e5-9e39-d3b532c10a28'
 
-@app.route('/')
-def index():
-    cookie = request.cookies.get("game_session")
-    print("User connected:", cookie)
-    return render_template("homepage.html")
-
 @app.route('/cookie')
 def create_cookie():
     cookie = request.cookies.get("game_session")
@@ -28,6 +22,31 @@ def create_cookie():
         return res
     else:
         return make_response(redirect('/'))
+
+@app.route('/resetcookie')
+def reset_cookie():
+    res = make_response(redirect('/'))
+    unique_cookie = generate_random_cookie()
+    session.add(UserSessions(user_cookie=unique_cookie))
+    session.commit()
+    res.set_cookie("game_session", unique_cookie, max_age=60*60*24*365*2)
+    return res
+
+@app.route('/')
+def index():
+    cookie = request.cookies.get("game_session")
+    print(cookie)
+    if not cookie:
+        res = make_response(render_template("homepage.html"))
+        unique_cookie = generate_random_cookie()
+        session.add(UserSessions(user_cookie=unique_cookie))
+        session.commit()
+        res.set_cookie("game_session", unique_cookie, max_age=60*60*24*365*2)
+        return res
+    else:
+        print("User connected:", cookie)
+        return render_template("homepage.html")
+
 
 @app.route("/choose_name", methods=['GET'])
 def display_name():
