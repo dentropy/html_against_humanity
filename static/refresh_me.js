cb5db886d8d9e9576e00b8a498957d893a076961
Paul Mullins <paulmullins@protonmail.com>
2020-04-05 17:37:13 -0400

    Added refresh javascript to pages that require it

---
+++ static/refresh_me.js
@@ -0,0 +1,7 @@
+
+// https://stackoverflow.com/questions/2787679/how-to-reload-page-every-5-seconds
+// This class is for python developerment not javascript
+// FIGHT ME
+setTimeout(function(){
+    window.location.reload(1);
+ }, 3000);
